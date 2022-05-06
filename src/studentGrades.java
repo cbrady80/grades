d02e92096690447819f3214483448936d1b851c5
Cheryl Brady <cbrady80@byui.edu>
2022-05-06 15:19:20 -0600

    first commit

---
+++ src/studentGrades.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class studentGrades {
+
+    public static void main(String[] args) {
+        // Create a Scanner for user input
+        Scanner input = new Scanner(System.in);
+
+        // Explanation upon start of program.
+        System.out.println("This program will accept a list of student scores, " +
+                "and tell you your letter grade.");
+
+        // Create an arrayList
+        ArrayList<Double> scoresList = new ArrayList<>();
+
+        // Prompt for scores.
+        System.out.print("Enter a number score: ");
+
+        // Save scores in the arrayList
+
+        // Get scores average
+
+        // Compute letter grade
+
+        // Compute grade sign.
+
+        // Display the letter grade.
+
+    }
+
+
+}
